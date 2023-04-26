84267647345ff3a86bfd6f9d99b1fc9aee713429
f.abidli <abidliifiras@gmail>
2023-04-26 08:52:18 +0100

    26/04

--- src/components/Formulaire.vue
+++ src/components/Formulaire.vue
@@ -3,12 +3,13 @@
     <form class="my-4">
       <h3>{{ currentCategory.category }}</h3>
       <div v-for="(question, index) in currentCategory.questions" :key="index" class="mb-3">
+      <div v-if ="question.response===null " >
         <p>{{ question.question }}</p>
         <div v-if="question.type === 'text'">
-          <input type="text" :required="question.required" style="200 px" />
+          <input type="text" :required="question.required"  />
         </div>
         <div v-else-if="question.type === 'textarea'">
-          <textarea :required="question.required" style="200 px"></textarea>
+          <textarea :required="question.required" ></textarea>
         </div>
         <div v-else-if="question.type === 'select'">
           <select :required="question.required">
@@ -46,6 +47,23 @@
         <div v-else-if="question.type === 'paragraph'">
           <textarea :required="question.required" style="200 px"></textarea>
         </div>
+    </div>
+    <div v-else>
+        <p>{{ question.question }}</p>
+        <div v-if="question.type === 'radio_group'">
+          <div v-for="(option, index) in question.options" :key="index">
+            <input
+              type="radio"
+              :id="option.id"
+              :value="option.id"
+              :name="'question_' + question.id"
+              :required="question.required"
+              :checked="option.option == question.response"            />
+            <label :for="option.id" >{{ option.option }}</label>
+          </div>
+        </div>
+
+    </div>
       </div>
       <button type="submit" class="btn btn-primary" @click.prevent="submit()">Submit</button>
     </form>
